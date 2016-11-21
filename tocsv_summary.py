d426a0fcd87be9803198e959632574b53ada0edb
Warwick Stone <uozu.aho@gmail.com>
2016-11-21 21:14:47 +1100

    csv summary now includes last tracker stats

--- tocsv_summary.py
+++ tocsv_summary.py
@@ -64,7 +64,7 @@ class ReplaySummary:
     def get_row(self):
         replay = yasc2replay.load(self.replay_path, include_game_data=False)
         winner_id = self._get_winner_id(replay)
-        return {
+        rowdata = {
             'p0.name': replay.players[0].name,
             'p0.race': replay.players[0].race,
             'p1.name': replay.players[1].name,
@@ -74,12 +74,31 @@ class ReplaySummary:
             'winner_name': replay.players[winner_id].name,
             'gameloops': replay.replay_length_gameloops
         }
+        last_stat_events = self._get_last_tracker_stats_events(replay)
+        rowdata.update(self._tracker_stats_events_to_row_data(last_stat_events))
+        return rowdata
 
     def _get_winner_id(self, replay):
         """ Returns the index of the winning player """
         for i, player in enumerate(replay.players):
             if player.won:
                 return i
+
+    def _tracker_stats_events_to_row_data(self, events):
+        data = {}
+        for player_id, event in events.items():
+            for stat, value in event.data['m_stats'].items():
+                data['p{}.{}'.format(player_id, stat)] = value
+        return data
+
+    def _get_last_tracker_stats_events(self, replay):
+        # last tracker stats event per player
+        last_events = {}
+        for event in replay.get_tracker_events():
+            if event.data['_event'] == 'NNet.Replay.Tracker.SPlayerStatsEvent':
+                player_id = event.data['m_playerId']
+                last_events[player_id] = event
+        return last_events
 
 
 if __name__ == '__main__':
